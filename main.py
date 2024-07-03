5cf42cfa2617909135cc88bab311a7b522f24bb7
Tarek Poltermann <tarekjpoltermann@gmail.com>
2024-07-03 23:53:41 +0200

    Converting of whole folder

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
+import os
+
 address = 0x200000
 size = 65536
 byte_count = 32
 
-file = "C:/Users/tarek/Desktop/E39/data/GD20/A7570735.0DA"
+folder = "C:/Users/tarek/Desktop/E39/data/GD20/"
 
-data = {}
-binary_data = bytearray()
+os.makedirs("output", exist_ok=True)
 
-with open(file, "r") as data_file:
-    bmw_data = data_file.read()
-
-address_length = len(hex(address)[2:])
-data_file_name = "output"
-
-for line in bmw_data.splitlines():
-    if line.startswith(";;K_File-Name:"):
-        data_file_name = line.replace(";;K_File-Name:", "").strip()
-
-    if not line.startswith(":"):
+for file in os.listdir(folder):
+    if not str(file).lower().endswith(".0da"):
         continue
 
-    line_address = int(line[1:address_length + 1], 16)
+    data = {}
+    binary_data = bytearray()
 
-    if line_address < address or line_address > address + size:
-        continue
+    with open(os.path.join(folder, str(file)), "r") as data_file:
+        bmw_data = data_file.read()
+        data_file.close()
 
-    line_data = line[address_length + 3:-2]
-    line_bytes = [(int(line_data[i:i + 2], 16)) for i in range(0, len(line_data), 2)]
-    data[line_address] = line_bytes
+    address_length = len(hex(address)[2:])
+    data_file_name = "output"
 
-for address in range(address, address + size, byte_count):
-    line_data = data.get(address)
+    for line in bmw_data.splitlines():
+        if line.startswith(";;K_File-Name:"):
+            data_file_name = line.replace(";;K_File-Name:", "").strip()
 
-    if line_data is None:
-        continue
+        if not line.startswith(":"):
+            continue
 
-    for line_byte in line_data:
-        binary_data.append(line_byte)
+        line_address = int(line[1:address_length + 1], 16)
 
-with open(f"{data_file_name}.BIN", "wb") as binary_file:
-    binary_file.write(binary_data)
+        if line_address < address or line_address > address + size:
+            continue
+
+        line_data = line[address_length + 3:-2]
+        line_bytes = [(int(line_data[i:i + 2], 16)) for i in range(0, len(line_data), 2)]
+        data[line_address] = line_bytes
+
+    for adr in range(address, address + size, byte_count):
+        line_data = data.get(adr)
+
+        if line_data is None:
+            continue
+
+        for line_byte in line_data:
+            binary_data.append(line_byte)
+
+    with open(f"output/{data_file_name}.BIN", "wb") as binary_file:
+        binary_file.write(binary_data)
+        binary_file.close()
